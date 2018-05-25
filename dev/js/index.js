ecf65a6839ccd387490015b415d21d7f23e49ad6
Jorge Jacques <jhack@gmail.com>
2018-05-25 11:46:00 -0500

    improve base layout

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,52 +1,39 @@
 $( document ).ready(function() {
-	$(".wrap_desktop").camera({
-      loader: "none",
-      navigation: false,
-      fx: 'scrollHorz',
-      pagination: true,
-      height:'700',
-      alignment: 'center',
-      autoAdvance: true,
-      hover: false,
-      playPause: false,
-      time: 3000,
-      alignment: 'center',
-      // pauseOnClick: false
-   });  
 
-   $(".wrap_mobile").camera({
-      loader: "none",
-      navigation: false,
-      fx: 'scrollHorz',
-      pagination: true,
-      alignment: 'center',
-      autoAdvance: true,
-      hover: false,
-      playPause: false,
-      time: 3000,
-      alignment: 'center',
-      // pauseOnClick: false
-   }); 
-
-  if($(".sidebars").length != 0)
-  {
-    // All sides
-    var sides = ["left", "top", "right", "bottom"];
-    //$("h1 span.version").text($.fn.sidebar.version);
-  
-    // Initialize sidebars
-    for (var i = 0; i < sides.length; ++i) {
-        var cSide = sides[i];
-        $(".sidebar." + cSide).sidebar({side: cSide});
-    }
-
-    // Click handlers
-    $(".btn[data-action]").on("click", function () {
-        var $this = $(this);
-        var action = $this.attr("data-action");
-        var side = $this.attr("data-side");
-        $(".sidebar." + side).trigger("sidebar:" + action);
-        return false;
+    $( '#home-sidebar' ).sliderPro({
+        width: '100%',
+        height: 300,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        autoplay: true,
+        autoplayDelay: 8000,
+        autoScaleLayers: false,
+        breakpoints: {
+            360: {
+                height: 500
+            },
+            640: {
+                height: 300
+            }
+        }
     });
-}
+
+    if($(".sidebars").length != 0)
+    {
+        var sides = ["left", "top", "right", "bottom"];
+      
+        for (var i = 0; i < sides.length; ++i) {
+            var cSide = sides[i];
+            $(".sidebar." + cSide).sidebar({side: cSide});
+        }
+
+        $(".sidebar-btn[data-action]").on("click", function () {
+            var $this = $(this);
+            var action = $this.attr("data-action");
+            var side = $this.attr("data-side");
+            $(".sidebar." + side).trigger("sidebar:" + action);
+            return false;
+        });
+    }
 });
