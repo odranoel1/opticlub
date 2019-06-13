c014c14060cde746cba65adde51f02c1abe545bf
Leo Gonzalez <leo.gonzalez@lava.mx>
2019-06-13 18:56:57 -0500

    Add responsive: PartnerClub, Contact, Product, ProductDetail

--- dev/js/index.js
+++ dev/js/index.js
@@ -19,10 +19,32 @@ $( document ).ready(function() {
         }
     });
 
-    $('#product-carousel').owlCarousel({
-      nav: true,
-      dots: false,
+    $('#products-carousel').owlCarousel({
+        // items: 4,
+        nav: true,
+        dots: false,
+        loop: true,
+        responsive: {
+          0: {
+            nav: false,
+            dots: true,
+            items: 1
+          },
+          480: {
+            nav: false,
+            dots: true,
+            items: 2
+          },
+          768: {
+            items: 3
+          },
+          992: {
+            items: 3
+          }
+        }
     });
+    $( ".owl-prev").html('<i class="fas fa-chevron-left"></i>');
+    $( ".owl-next").html('<i class="fas fa-chevron-right"></i>');
 
     $('#video-carousel').owlCarousel({
       nav: true,
